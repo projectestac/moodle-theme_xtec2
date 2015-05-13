92ae92bdd00469fd82e4990d2f442ef88adcfe8d
pau.ferrer-ocana <crazyserver@gmail.com>
2015-05-13 12:49:01 +0200

    Change popup layout

--- config.php
+++ config.php
@@ -141,7 +141,7 @@ $THEME->layouts = array(
 
     // Pages that appear in pop-up windows - no navigation, no blocks, no header.
     'popup' => array(
-        'file' => 'general.php',
+        'file' => 'popup.php',
         'regions' => array(),
         'options' => array('nofooter' => true, 'nonavbar' => true),
     ),
